7a5f9bc1ab02c7c2d67fa109dbe3741a487fddb3
acque.2two@gmail.com <acque.2two@gmail.com>
2016-12-24 16:11:24 +0000

    JS Fixed

--- custom.js
+++ custom.js
@@ -1,14 +1,18 @@
 $(function(){
+    var script;
     $('head link:last').after('<link rel="stylesheet" href="/uploads/bootstrap.min.css">');
     
-    var script = document.createElement("script");
+    script = document.createElement("script");
+    script.src = "/uploads/jquery-3.1.1.min.js";
+    document.getElementsByTagName('head')[0].appendChild(script);
+    
+    script = document.createElement("script");
     script.src = "/uploads/bootstrap.min.js";
     document.getElementsByTagName('head')[0].appendChild(script);
+    
     script = document.createElement("script");
     script.src = "/uploads/mermaid.min.js";
     document.getElementsByTagName('head')[0].appendChild(script);
-    
-    $('head script:last').after('<script>mermaid.initialize({startOnLoad:true});</script>');
     
     
     $('#head').addClass('navbar navbar-fixed-top navbar-default navbar-header');
