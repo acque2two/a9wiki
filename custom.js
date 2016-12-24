40a63a3694be976f9e7e66ce563ff364970cc576
acque.2two@gmail.com <acque.2two@gmail.com>
2016-12-24 07:16:17 +0000

    Navbar setted

--- custom.js
+++ custom.js
@@ -1,4 +1,7 @@
 $(function(){
     $('head link:last').after('<link rel="stylesheet" href="/uploads/bootstrap.min.css">');
     $('head link:last').after('<script type="text/javascript" src="/uploads/bootstrap.min.js"></script>');
+    
+    $('#head').obj.addClass('navbar navbar-fixed-top');
+    
 });
