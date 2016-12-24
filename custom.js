5935f982a47d2cb9391ad097755657a941cbc920
acque.2two@gmail.com <acque.2two@gmail.com>
2016-12-24 07:07:01 +0000

    Test deleted

--- custom.js
+++ custom.js
@@ -1,5 +1,4 @@
 $(function(){
     $('head link:last').after('<link rel="stylesheet" href="/uploads/bootstrap.min.css">');
     $('head link:last').after('<script type="text/javascript" src="/uploads/bootstrap.min.js"></script>');
-    $('head link:last').after('<script type="text/javascript" src="/uploads/test.js"></script>');
 });
