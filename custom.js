7570a7f6161d3b8e797dbe49e27ac40c24d053ee
acque.2two@gmail.com <acque.2two@gmail.com>
2016-12-24 07:06:26 +0000

    JSLOAD TEST

--- custom.js
+++ custom.js
@@ -1,4 +1,5 @@
 $(function(){
     $('head link:last').after('<link rel="stylesheet" href="/uploads/bootstrap.min.css">');
-    $('head script:last').after('<script type="text/javascript" src="/uploads/bootstrap.min.js"></script>');
+    $('head link:last').after('<script type="text/javascript" src="/uploads/bootstrap.min.js"></script>');
+    $('head link:last').after('<script type="text/javascript" src="/uploads/test.js"></script>');
 });
