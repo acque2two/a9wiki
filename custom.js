5c8425a2ece48bfb81e27891b347b5d000ebf896
acque.2two@gmail.com <acque.2two@gmail.com>
2016-12-24 07:33:48 +0000

    test

--- custom.js
+++ custom.js
@@ -2,6 +2,9 @@ $(function(){
     $('head link:last').after('<link rel="stylesheet" href="/uploads/bootstrap.min.css">');
     $('head link:last').after('<script type="text/javascript" src="/uploads/bootstrap.min.js"></script>');
     
-    $('#head').addClass('navbar navbar-fixed-top');
+    $('#head').addClass('navbar navbar-fixed-top navbar-default navbar-header');
+    $('#head h1').addClass('navbar-brand');
+    var title = $('#head h1').html();
+    $('#head').prepend('<a class="navbar-brand">' + title + '</a>');
     
 });
