4fd852695d764494a015d6f3320ddfbbc98162d3
acque.2two@gmail.com <acque.2two@gmail.com>
2016-12-24 07:04:08 +0000

    js fixed

--- custom.js
+++ custom.js
@@ -1,4 +1,4 @@
 $(function(){
     $('head link:last').after('<link rel="stylesheet" href="/uploads/bootstrap.min.css">');
-    $('head script:last').after('<script type="text/javascript" src="/javascript/jquery-1.7.2.min.js"></script>');
+    $('head script:last').after('<script type="text/javascript" src="/uploads/bootstrap.min.js"></script>');
 });
